f68f1a87dd6a07bd0b000567cf9021bd8f06e899
luiggy macias <ferrinluiggy@gmail.com>
2022-04-06 13:40:34 -0500

    adding css to the buttons

--- src/App.tsx
+++ src/App.tsx
@@ -17,36 +17,56 @@ function App() {
       setLoop(false);
       setTimeout(() => {
         setVideo(video3);
-        setLoop(true)
+        setLoop(true);
       }, 4000);
     } else if (video === video3) {
       setVideo(video4);
       setLoop(false);
       setTimeout(() => {
         setVideo(video1);
-        setLoop(true)
+        setLoop(true);
       }, 4000);
     }
   };
 
   return (
     <div className="App">
-      <h1>hello</h1>
+      {/* <h1>hello</h1> */}
       {/* <video width="750" height="500" controls>
         <source src={video1} type="video/mp4" />
       </video> */}
-      <button onClick={changeVideo}>
+      <button
+        onClick={changeVideo}
+        className='btn btn-compact-footprint'
+        style={{
+          display: video === video1 ? "block" : "none",
+        }}
+      >
         {video === video3 ? "return to normal" : "compact footprint"}
       </button>
-      <div>{video === video3 && <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed molestias officia magni eius ipsam voluptatibus similique dolorum eligendi nesciunt enim? Tempore hic illum ullam. Quos nulla veritatis iusto itaque ullam.</p>}</div>
+      <div className="description">
+        {video === video3 && (
+          <>
+            <button className="btn">Compact footprint</button>
+            <p>
+              Smallest, fully contained palletizing unit for a single pallet and load/unload function utilizing a pallet jack or forklift
+            </p>
+          </>
+        )}
+      </div>
       <ReactPlayer
         url={video}
-        width={"80%"}
-        height="80%"
+        width={"65%"}
+        height="65%"
         loop={loop}
         playing={true}
         controls
       />
+      {video === video3 && (
+          <>
+            <button className="btn btn-back-to-features" onClick={changeVideo}>Back to Features</button>
+          </>
+        )}
     </div>
   );
 }
